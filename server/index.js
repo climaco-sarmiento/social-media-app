031b9e043ec1645a240d9975b5d69efef5139de3
climaco-sarmiento <118277981+climaco-sarmiento@users.noreply.github.com>
2023-05-10 16:14:56 +0100

    update

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
